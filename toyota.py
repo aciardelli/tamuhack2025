86b2472af14dedd9d4f771973e38274eef7afbb4
aciardelli <anthciardelli@gmail.com>
2025-01-25 16:05:25 -0600

    changed

--- toyota.py
+++ toyota.py
@@ -4,39 +4,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
 import time
+from selenium.webdriver.firefox.options import Options
+
+profile_path = r"/Users/aciardelli/Library/Application Support/Firefox/Profiles/o9yia4xu.default-release"
+options = Options()
+options.set_preference('profile', profile_path)
+options.set_preference('browser.startup.homepage_override.mstone', '')
+options.set_preference('startup.homepage_welcome_url.additional', '')
+options.add_argument('-profile')
+options.add_argument(profile_path)
+mydriver = webdriver.Firefox(options=options)
 
 url = "https://carsheet.io/toyota/"
-mydriver = webdriver.Firefox()
+mydriver.get(url)
+time.sleep(3)
 
 try:
-    mydriver.get(url)
-    time.sleep(5)
-    
-    # First try to find and close the overlapping filters button
-    try:
-        filters_btn = mydriver.find_element(By.ID, "cs-btn-filters")
-        filters_btn.click()
-    except:
-        print("Could not find filters button")
-    
     wait = WebDriverWait(mydriver, 10)
     tbody = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "tbody")))
     rows = tbody.find_elements(By.TAG_NAME, "tr")
-    
     for row in rows:
-        try:
-            clickable = row.find_element(By.CSS_SELECTOR, "td")
-            mydriver.execute_script("arguments[0].scrollIntoView(true);", clickable)
-            time.sleep(1)
-            mydriver.execute_script("arguments[0].click();", clickable)
-            print("clicked row element")
-            time.sleep(1)
-        except Exception as e:
-            print(f"Error clicking row: {e}")
-            continue
+        print(row.get_attribute('innerHTML'))
 
-except Exception as e:
-    print(f"Major error: {e}")
-finally:
-    if mydriver:
-        mydriver.quit()
+except:
+    print("There was an error")
